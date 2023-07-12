63b6a98e3a1ee57c0a79a934bebde688a74cd19c
Yuanxyyds <liuhongyuan.liu@mail.utoronto.ca>
2023-07-12 14:32:26 -0400

    feat: add complete experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
-import { AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
-import { GrLocation } from 'react-icons/gr';
 import CommonComponent from '@/app/helpers/common-component';
+import Link from 'next/link';
 
 
 
@@ -21,17 +19,19 @@ export default function ExperienceContent() {
                                 <p className=' text-white text-xs mr-3 ml-10 text-right mt-2'> - Teaching the ideas of solving tough math/computer science/physics questions to high school students. </p>
                                 <p className=' text-white text-xs mr-3 ml-10 text-right mt-1'> - Assisting students in finding useful study methods to enhance academic performance. </p>
                             </div>
+                            <Link href='#' className='right-link1'> Link</Link>
                         </div>
                         <div className=' experience-container'>
                             <h1 className=' experience-header2 text-center mt-1'>Sep 2020 - Jan 2021</h1>
                             <div className='left-experience-content1'>
                                 <h1 className=' experience-header ml-3'>UofT Food Truck</h1>
-                                <p className=' text-white text-xs ml-3 mr-10'> UofT Project | Mrs. Karan | Toronto</p>
+                                <p className=' text-white text-xs ml-3 mr-10'> UofT Project | Toronto</p>
                                 <p className=' text-white text-xs ml-3 mr-10 mt-2'> - Android App Design for the UofT Food Truck System. </p>
                                 <p className=' text-white text-xs ml-3 mr-10 mt-1'> - Participated in Java Backend Writing and XML frontend Designing </p>
                                 <p className=' text-white text-xs ml-3 mr-10 mt-1'> - Familar with Design Pattern and OOP architeture </p>
                                 <p className=' text-white text-xs ml-3 mr-10 mt-1'> - Data Serialization using SQLite </p>
                             </div>
+                            <Link href='#' className='left-link1'> Link</Link>
                         </div>
                         <div className=' experience-container'>
                             <h1 className=' experience-header3 text-center mt-1'>May 2022 - Current</h1>
@@ -42,6 +42,7 @@ export default function ExperienceContent() {
                                 <p className=' text-white text-xs mr-3 ml-10 text-right mt-1'> - Organizing weekly activities, managing members & departments, and making plans about club development. </p>
                                 <p className=' text-white text-xs mr-3 ml-10 text-right mt-1'> - Early startup business planning, business model testing, UX survey. </p>
                             </div>
+                            <Link href='#' className='right-link2'> Link</Link>
                         </div>
                         <div className=' experience-container'>
                             <h1 className=' experience-header4 text-center mt-1'>Jun 2022 - Current</h1>
@@ -52,6 +53,7 @@ export default function ExperienceContent() {
                                 <p className=' text-white text-xs ml-3 mr-10 mt-1'> - Version 2: Rebuild website using ReactJS (NextJS) and Tailwind CSS </p>
                                 <p className=' text-white text-xs ml-3 mr-10 mt-1'> - Write functional JavaScript and Djongo API</p>
                             </div>
+                            <Link href='#' className='left-link2'> Link</Link>
                         </div>
                         <div className=' experience-container'>
                             <h1 className=' experience-header5 text-center mt-1'>Feb 2023 - May 2023</h1>
@@ -63,6 +65,31 @@ export default function ExperienceContent() {
                                 <p className=' text-white text-xs mr-3 ml-10 text-right mt-1'> - Build Prediction Model and Interactive visual plots </p>
                                 <p className=' text-white text-xs mr-3 ml-10 text-right mt-1'> - Build Rmarkdown website for project report</p>
                             </div>
+                            <Link href='#' className='right-link3'> Link</Link>
+                        </div>
+                        <div className=' experience-container'>
+                            <h1 className=' experience-header6 text-center mt-1'>May 2023 - Current</h1>
+                            <div className='left-experience-content3'>
+                                <h1 className=' experience-header ml-3'>Campus Eats</h1>
+                                <p className=' text-white text-xs ml-3 mr-10'> Self-Project | Project Lead | Toronto</p>
+                                <p className=' text-white text-xs ml-3 mr-10 mt-2'> - An update version of the UofT Food truck project developed by Flutter, supporting IOS/Android. </p>
+                                <p className=' text-white text-xs ml-3 mr-10 mt-1'> - Strong understanding in Futter packages and firebase backend </p>
+                                <p className=' text-white text-xs ml-3 mr-10 mt-1'> - Participate in Figma UI design</p>
+                                <p className=' text-white text-xs ml-3 mr-10 mt-1'> - Project Lead, teach and assign weekly tasks to team members</p>
+                            </div>
+                            <Link href='#' className='left-link3'> Link</Link>
+                        </div>
+                        <div className=' experience-container'>
+                            <h1 className=' experience-header7 text-center mt-1'>May 2023 - May 2024</h1>
+                            <div className='right-experience-content4'>
+                                <h1 className=' experience-header mr-3 ml-10 text-right'>Cloud & MobApp dev</h1>
+                                <p className=' text-white text-xs mr-3 ml-10 text-right'> Co-op | Johnson Controls | Toronto </p>
+                                <p className=' text-white text-xs mr-3 ml-10 text-right mt-2'> - Develop home security app by Flutter, supporting IOS/Android </p>
+                                <p className=' text-white text-xs mr-3 ml-10 text-right mt-1'> - Backend developing with firebase and flutter</p>
+                                <p className=' text-white text-xs mr-3 ml-10 text-right mt-1'> - Experienced using Google Cloud Console</p>
+                                <p className=' text-white text-xs mr-3 ml-10 text-right mt-1'> - Good understanding in gradle/Xcode building</p>
+                            </div>
+                            <Link href='#' className='right-link4'> Link</Link>
                         </div>
 
                     </section>
@@ -75,6 +102,8 @@ export default function ExperienceContent() {
                         <div className=' horizontal-line'></div>
                         <div className=' horizontal-line'></div>
                         <div className=' horizontal-line'></div>
+                        <div className=' horizontal-line'></div>
+                        <div className=' horizontal-line'></div>
                     </section>
                     {/* Center Line Section */}
                     <section>
@@ -82,9 +111,15 @@ export default function ExperienceContent() {
                         <div className='center-line'> </div>
                         <div className='center-line'> </div>
                         <div className='center-line'> </div>
+                        <div className='center-line'> </div>
+                        <div className='center-line'> </div>
                     </section>
                     {/* Ball Section */}
                     <section>
+                        <div className='ball'> </div>
+                        <div className='ball'> </div>
+                        <div className='ball'> </div>
+                        <div className='ball'> </div>
                         <div className='ball'> </div>
                         <div className='ball'> </div>
                         <div className='ball'> </div>
